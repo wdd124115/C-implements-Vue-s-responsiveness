97a3d3303541a50534a664bccb0d91f00b1632f2
wdd124115 <116279239+wdd124115@users.noreply.github.com>
2024-08-21 17:17:46 +0800

    被观察者属性的变化
    通过这个类就能知道被观察者的属性新的值和旧的值了。
    Through this class, you can know the new value and the old value of the observed property。

---
+++ ObservableProperty.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+public class ObservableProperty<T>
+{
+    private T _value;
+    private readonly Observable _observable;
+    private readonly string _propertyName;
+
+    public ObservableProperty(T initialValue, Observable observable, string propertyName)
+    {
+        _value = initialValue;
+        _observable = observable;
+        _propertyName = propertyName;
+    }
+    public string PropertyName => _propertyName;
+
+    public T Value
+    {
+        get => _value;
+        set
+        {
+            if (!EqualityComparer<T>.Default.Equals(_value, value))
+            {
+                T oldValue = _value;
+                _value = value;
+                _observable.NotifyPropertyChange(_propertyName, oldValue, value);
+            }
+        }
+    }
+}
